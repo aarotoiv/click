d1b62d89aa02e8d3c3d7496761ca56c5ac729059
aarotoiv <aaro.toivonen@gmail.com>
2020-02-17 06:38:49 +0200

    client setups

---
+++ client/src/components/Main.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="main">
+    <p>asdfasdfs</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  mounted() {
+    console.log("AAA");
+  }
+}
+</script>
+
+
+<style scoped>
+  p {
+    margin: 0;
+    padding:0;
+  }
+
+</style>
